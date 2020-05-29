55b03eacf18cf1565e82496bdc90328ef3a34959
peryapeee <63729641+peryapeee@users.noreply.github.com>
2020-05-29 14:17:04 +0300

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -3,10 +3,22 @@ package test;
 import domain.Exercise;
 
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        /**
+         * Encrypting of english words
+         */
+        Exercise ex = new Exercise(97,26);
+        String enc = ex.encrypt("poltava","ppc");
+        System.out.println("English text - " + enc);
+        String dec = ex.decrypt(enc,"ppc");
+        System.out.println("Decrypted text - " + dec);
+        /**
+         * Encrypting of russian words
+         */
+        Exercise exercise = new Exercise(1072, 33);
+        enc = exercise.encrypt("полтава", "ппк");
+        System.out.println("Russian text - " + enc);
+        dec = exercise.decrypt(enc, "ппк");
+        System.out.println("Decrypted text - " + dec);
     }
 }
